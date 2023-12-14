6e113aafa918a3149357c0716cd08ca1197b8678
Darli <n00167313@upn.pe>
2023-12-14 12:11:39 -0500

    Add Google Sans font and update styles

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import SectionHome from '@/components/Sections/SectionHome'
+export default function Home() {
+  return (
+    <>
+      <div className=' max-[712px]:pt-20 '>
+        <SectionHome />
+      </div>
+    </>
+  )
+}
